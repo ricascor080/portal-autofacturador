0ec148bb2e4cd84c9742fb4c74514aa7cfaf8d0c
Ricardo <ricascor080@gmail.com>
2025-08-19 11:47:32 -0600

    Proyecto Django inicial

---
+++ polls/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Choice, Question
+
+
+# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
